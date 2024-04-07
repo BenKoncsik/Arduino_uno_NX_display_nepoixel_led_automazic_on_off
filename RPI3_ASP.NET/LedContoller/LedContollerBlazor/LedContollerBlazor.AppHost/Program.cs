e2234bebe6222f986e892013538bc51885b4792f
Benedek Koncsik <koncsik.benedek.andras@gamil.com>
2024-04-07 21:52:45 +0200

    Add screen component

--- RPI3_ASP.NET/LedContoller/LedContollerBlazor/LedContollerBlazor.AppHost/Program.cs
+++
@@ -1,5 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.LedContollerBlazor>("ledcontollerblazor");
-
-builder.Build().Run();
--- RPI3_ASP.NET/LedContoller/LedContollerBlazor/LedContollerBlazor/LedContollerBlazor.Client/Model/LEDStateJsonModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LedContollerBlazor.Client.Model
-{
-    public class LEDStateJsonModel
-    {
-        public int LedNumber { get; set; }
-        public string LedColor { get; set; }
-        public float Brightness { get; set; }
-    }
-}
